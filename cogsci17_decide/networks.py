7627974e2b0c58819e20daf4de8d6020007a6125
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-12-13 18:20:22 -0500

    Add drift diffusion network.

--- cogsci17_decide/networks.py
+++ cogsci17_decide/networks.py
@@ -20,7 +20,7 @@ def UsherMcClelland(d, n_neurons, dt):
 
     with nengo.Network() as net:
         net.input = nengo.Node(size_in=d)
-        x = nengo.Ensemble(n_neurons, d)
+        x = nengo.Ensemble(d * n_neurons, d)
 
         for i in range(d):
             nengo.Connection(
@@ -33,5 +33,39 @@ def UsherMcClelland(d, n_neurons, dt):
             synapse=tau_actual)
 
         net.output = x
+
+    return net
+
+
+def DriftDiffusion(d, n_neurons, dt):
+    n_neurons = n_neurons // 2  # two-layered network
+
+    rec_synapse = 0.1
+    threshold = 0.8
+
+    with nengo.Network() as net:
+        with nengo.presets.ThresholdingEnsembles(0.):
+            evidence_accum = nengo.networks.EnsembleArray(n_neurons, d)
+            nengo.Connection(
+                evidence_accum.output, evidence_accum.input,
+                synapse=rec_synapse)
+            thresholding = nengo.networks.EnsembleArray(n_neurons, d)
+            thresholding.add_output('heaviside', lambda x: x > 0.)
+
+        bias = nengo.Node(1.)
+
+        net.input = nengo.Node(size_in=d)
+        net.output = nengo.Node(size_in=d)
+
+        nengo.Connection(
+            net.input, evidence_accum.input, transform=0.5,
+            synapse=rec_synapse)
+        nengo.Connection(evidence_accum.output, thresholding.input)
+        nengo.Connection(
+            bias, thresholding.input, transform=-threshold * np.ones((d, 1)))
+        nengo.Connection(
+            thresholding.heaviside, evidence_accum.input,
+            transform=-2. + 2.5 * np.eye(d), synapse=rec_synapse)
+        nengo.Connection(thresholding.heaviside, net.output, synapse=None)
 
     return net
