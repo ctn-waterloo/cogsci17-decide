9bc2382bc094a8001da480d6b92333932674f06d
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-12-14 17:07:19 -0500

    Add psyrun task to generate data.

--- psy-tasks/task_decision.py
+++ psy-tasks/task_decision.py
@@ -6,7 +6,7 @@ pspace = (
     Param(network=['DriftDiffusion', 'UsherMcClelland']) *
     Param(baseline=[0.1, 0.5, 0.8]) *
     Param(target_sep=[0.05, 0.1, 0.15, 0.2]) *
-    Param(noise=[0.0, 0.05, 0.1]) *
+    Param(noise=[0.0, 0.01, 0.02, 0.03, 0.04, 0.05]) *
     Param(seed=range(10))
 )
 
