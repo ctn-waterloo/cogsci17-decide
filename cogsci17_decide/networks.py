e09e2fa96a756e1999ef539302d453e59d97ad87
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-12-27 15:46:55 +0100

    Match drift diffusion implementation to WTA impl.

--- cogsci17_decide/networks.py
+++ cogsci17_decide/networks.py
@@ -38,34 +38,51 @@ def UsherMcClelland(d, n_neurons, dt):
 
 
 def DriftDiffusion(d, n_neurons, dt):
-    n_neurons = n_neurons // 2  # two-layered network
+    k = 0.
+    beta = 0.
+    tau_model = 0.01
 
-    rec_synapse = 0.1
+    tau_actual = 0.1
+    a = np.exp(-dt / tau_actual)
+
+    # eqn (4) ignoring truncation, put into the canonical form:
+    #   x[t+dt] = Ax[t] + Bu
+    inhibit = np.ones((d, d))
+    inhibit[np.diag_indices(d)] = 0.
+    I = np.eye(d)
+    B = dt / tau_model
+    A = (-k * I - beta * inhibit) * dt / tau_model + I
+
+    n_neurons_threshold = 50
+    n_neurons_x = n_neurons - n_neurons_threshold
+    assert n_neurons_x > 0
     threshold = 0.8
 
     with nengo.Network() as net:
-        with nengo.presets.ThresholdingEnsembles(0.):
-            evidence_accum = nengo.networks.EnsembleArray(n_neurons, d)
+        net.input = nengo.Node(size_in=d)
+        x = nengo.Ensemble(d * n_neurons, d)
+
+        for i in range(d):
             nengo.Connection(
-                evidence_accum.output, evidence_accum.input,
-                synapse=rec_synapse)
+                net.input[i], x[i],
+                transform=B / (1 - a),  # discrete principle 3
+                synapse=tau_actual)
+
+        nengo.Connection(
+            x, x, transform=(A - a * I) / (1 - a),  # discrete principle 3
+            synapse=tau_actual)
+
+        with nengo.presets.ThresholdingEnsembles(0.):
             thresholding = nengo.networks.EnsembleArray(n_neurons, d)
-            thresholding.add_output('heaviside', lambda x: x > 0.)
+            net.output = thresholding.add_output('heaviside', lambda x: x > 0.)
 
         bias = nengo.Node(1.)
 
-        net.input = nengo.Node(size_in=d)
-        net.output = nengo.Node(size_in=d)
-
-        nengo.Connection(
-            net.input, evidence_accum.input, transform=0.5,
-            synapse=rec_synapse)
-        nengo.Connection(evidence_accum.output, thresholding.input)
+        nengo.Connection(x, thresholding.input)
         nengo.Connection(
             bias, thresholding.input, transform=-threshold * np.ones((d, 1)))
         nengo.Connection(
-            thresholding.heaviside, evidence_accum.input,
-            transform=-2. + 2.5 * np.eye(d), synapse=rec_synapse)
-        nengo.Connection(thresholding.heaviside, net.output, synapse=None)
+            thresholding.heaviside, x,
+            transform=-2. + 2.5 * np.eye(d) / (1 - a), synapse=tau_actual)
 
     return net
