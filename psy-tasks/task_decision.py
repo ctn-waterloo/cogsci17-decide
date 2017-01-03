7fa85bdca4150d547a6d6261c4bf41189d71e8f6
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-01-03 15:28:49 -0500

    Update task with new parameters.

--- psy-tasks/task_decision.py
+++ psy-tasks/task_decision.py
@@ -3,7 +3,15 @@ from psyrun import Param
 from cogsci17_decide.trial import DecisionTrial
 
 pspace = (
-    Param(network=['DriftDiffusion', 'UsherMcClelland']) *
+    (
+        Param(
+            network=['UsherMcClelland'],
+            scale=[1.],
+            share_thresholding_intercepts=[False]) +
+        Param(network=['DriftDiffusion']) *
+        Param(share_thresholding_intercepts=[False, True]) *
+        Param(scale=[0.15, 1.])
+    ) *
     Param(baseline=[0.1, 0.5, 0.8]) *
     Param(target_sep=[0.05, 0.1, 0.15, 0.2]) *
     Param(noise=[0.0, 0.01, 0.02, 0.03, 0.04, 0.05]) *
