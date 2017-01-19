635ae4f0c6c7fbc4b037d9857e1be37c48d79eda
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-01-19 11:20:01 -0500

    Update simulation parameters.

--- psy-tasks/task_decision.py
+++ psy-tasks/task_decision.py
@@ -10,12 +10,12 @@ pspace = (
             share_thresholding_intercepts=[False]) +
         Param(network=['DriftDiffusion']) *
         Param(share_thresholding_intercepts=[False, True]) *
-        Param(scale=[0.15, 1.])
+        Param(scale=[0.2, 1.])
     ) *
-    Param(baseline=[0.1, 0.5, 0.8]) *
+    Param(baseline=[0.2, 0.6, 1.0]) *
     Param(target_sep=[0.05, 0.1, 0.15, 0.2]) *
     Param(noise=[0.0, 0.01, 0.02, 0.03, 0.04, 0.05]) *
-    Param(seed=range(10))
+    Param(seed=range(50))
 )
 
 min_items = 1
