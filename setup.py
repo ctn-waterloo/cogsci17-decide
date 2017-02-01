ce41f35b1a6ab1b683631745395979669eaae9ab
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-02-01 13:26:27 -0500

    Update setup.py with dependencies.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="cogsci17_decide",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
-        'nengo',
+        'matplotlib < 2.0',
+        'nengo >= 2.3, < 3.0',
         'numpy',
-        'matplotlib',
-        'pytry'
+        'pandas',
+        'pytry',
+        'seaborn',
     ]
 )
